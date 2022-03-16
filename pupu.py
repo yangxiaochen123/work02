553316ef1d698517064170584b01e5ce11dc7b5f
yangxiaochen123 <728769148@qq.com>
2022-03-16 18:29:37 +0800

    第一版朴朴

---
+++ pupu.py
@@ -0,0 +1,29 @@
+import json
+import time
+import requests
+
+def request_url():
+    url = 'https://j1.pupuapi.com/client/product/storeproduct/detail/deef1dd8-65ee-46bc-9e18-8cf1478a67e9/ed60af11-25b0-48b8-bc5b-f9136d9f89ad'
+    head = {
+        'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36 MicroMessenger/7.0.9.501 NetType/WIFI MiniProgramEnv/Windows WindowsWechat'
+    }
+
+    respnse = requests.get(url, headers=head,verify=False)
+
+    dict = json.loads(respnse.text)  #将字符串转为字典
+    name = dict["data"]["name"]  #得到商品名字
+    spec = dict["data"]["spec"]  #得到规格
+    price = str(int(dict["data"]["price"]) / 100)  #得到折扣价
+    market_price = str(int(dict["data"]["market_price"]) / 100)  #得到原价
+    share_content = dict["data"]["share_content"]  #得到详细内容
+    #将得到内容输出
+    print("-------------商品：" + name + "-------------")
+    print("规格：" + spec)
+    print("原价：" + price)
+    print("原价/折扣价：" + price + "/" + market_price)
+    print("详细内容：" + share_content)
+    print('\n')
+
+#调用方法
+if __name__ == '__main__':
+    request_url()
